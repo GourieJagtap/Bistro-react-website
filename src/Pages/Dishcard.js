5afaba3d79c12dd1d847fe20813e8a6ce5df5641
apple <apple@MacBook-Air.local>
2023-02-06 02:45:58 +0530

    second commit

--- src/Pages/Dishcard.js
+++ src/Pages/Dishcard.js
@@ -1,27 +1,27 @@
-import { Paper } from '@mui/material'
+import { Button, Paper } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React from 'react';
+import "./Dishcard.css"
 
 const Dishcard = (props) => {
   return (
-    <div>
-        <Paper>
-            <Box>
-                <div class="row">
-                    <div class="col-sm-12 d-flex justify-content-center align-item-center">
-                        <img src=""></img>
-                    </div>
-                    <div class="col-sm-12 d-flex justify-content-center align-item-center">
-                        <p>{props.title}</p>
-                    </div>
-                    <div class="col-sm-12 d-flex justify-content-center align-item-center">
-                        <p>{props.desp}</p>
-                    </div>
-                    <div class="col-sm-12 d-flex justify-content-center align-item-center">
-                        <p>price</p>
-                    </div>
-                </div>
-            </Box>
+    <div class="col-md-4 font2">
+        <Paper elevation={20} sx={{padding:"10px"}}>
+            <div class="col-sm-12 d-flex justify-content-center align-item-center">
+                <img src={props.img} class="img1"></img>
+            </div>
+            <div class="col-sm-12 d-flex justify-content-center align-item-center title">
+                <p>{props.title}</p>
+            </div>
+            <div class="col-sm-12 d-flex justify-content-center align-item-center desp">
+                <p><center>{props.desp}</center></p>
+            </div>
+            <div class="col-sm-12 d-flex justify-content-center align-item-center prc" >
+                <p>{props.price}</p>
+            </div>
+            <div class="col-sm-12 d-flex justify-content-center align-item-center p-2">
+                <Button variant="outlined" color="error" sx={{color:"black"}}>Add to Cart</Button>
+            </div>
         </Paper>
     </div>
   )
--- src/Pages/Dishes.js
+++ src/Pages/Dishes.js
@@ -1,43 +1,63 @@
 import React from 'react';
 import Dishcard from './Dishcard';
 import "./Dishes.css";
+import burger from "../Images/burger.jpg"
+import pasta from "../Images/pasta.jpg"
+import pizza from "../Images/pizza.jpg"
 
 
 const Dishes = () => {
     let dishList=[
         {
-            img:"",
+            dimg:burger,
             dtitle:"Cheese Royale Burger",
-            ddesp:"Grilled Beef Patty, Russian Musuels and relished burger"
+            ddesp:"Grilled Beef Patty, Russian Musuels and relished burger",
+            dprice:"RS 149"
         },
         {
-            img:"",
-            dtitle:"Prawns Havana Pasta"
+            dimg:pasta,
+            dtitle:"Prawns Havana Pasta",
+            ddesp:"Smoke Basil,Mosarella and picorine romanio",
+            dprice:"RS 179",
         },
         {
-            img:"",
-            dtitle:"Chicken Mossarella"
+            dimg:pizza,
+            dtitle:"Chicken Pizza",
+            ddesp:"Mosarella with Marinated Chicken and Basil freshno ",
+            dprice:"RS 199"
         },
     ]
 
   return (
-    <div class="container-fluid">
-    <div class="row fontt">
-        <div class="col-sm-12 d-flex justify-content-center pt-5 dish">
-            Popular Dishes
+    <div class="container-fluid ">
+        <div class="row fontt" >
+            <div class="col-sm-12 d-flex justify-content-center pt-5 dish">
+                <center>Popular Dishes</center>
+            </div>
+            <div class="col-sm-12 d-flex justify-content-center grey">
+                <center>Try the delicious new dishes from our chef.</center>
+            </div>
         </div>
-        <div class="col-sm-12 d-flex justify-content-center ">
-            Try the delicious new dishes from our chef.
+        <div class="row " >
+                <Dishcard 
+                    img={dishList[0].dimg}
+                    title={dishList[0].dtitle} 
+                    desp={dishList[0].ddesp}
+                    price={dishList[0].dprice}>
+                </Dishcard>
+                <Dishcard 
+                    img={dishList[1].dimg}
+                    title={dishList[1].dtitle} 
+                    desp={dishList[1].ddesp}
+                    price={dishList[1].dprice}>
+                </Dishcard>
+                <Dishcard 
+                    img={dishList[2].dimg}
+                    title={dishList[2].dtitle} 
+                    desp={dishList[2].ddesp}
+                    price={dishList[2].dprice}>
+                </Dishcard>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6 d-flex justify-content-center ">
-            <Dishcard title={dishList[0].dtitle} desp={dishList[0].ddesp}></Dishcard>
-            <Dishcard title={dishList[1].dtitle} ></Dishcard>
-            <Dishcard title={dishList[2].dtitle} ></Dishcard>
-
-        </div>
-    </div>
     </div>
   )
 }
